6632204a72c25cd5b4386b09a77ebb2f2a14ce46
julian-urbano <urbano.julian@gmail.com>
2022-05-30 17:28:17 +0200

    Add Rstudio config.

---
+++ 03-rstudio/rstudio_backup.bat
@@ -0,0 +1,3 @@
+xcopy /Y "C:\Program Files\RStudio\resources\themes\caerolus.rstheme" .
+xcopy /Y /E "%appdata%\RStudio\*" .\settings\
+pause
---
+++ 03-rstudio/rstudio_restore.bat
@@ -0,0 +1,3 @@
+xcopy /Y .\caerolus.rstheme "C:\Program Files\RStudio\resources\themes"
+xcopy /Y /E .\settings\* "%appdata%\RStudio\"
+pause
